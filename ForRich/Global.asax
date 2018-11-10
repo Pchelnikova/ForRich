eee58533a541608483369798a600f2c29eaceba1
Irina <pchelnikova23@gmail.com>
2018-11-11 00:15:12 +0200

    Add project files.

---
+++ ForRich/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ForRich.MvcApplication" Language="C#" %>
---
+++ ForRich/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ForRich.Startup))]
+namespace ForRich
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
